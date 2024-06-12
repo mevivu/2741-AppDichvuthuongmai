614bdea5c7ce8352cb473bd36b41409024908e8c
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-06-12 14:53:56 +0700

    create list area

--- app/Admin/DataTables/Area/AreaDataTable.php
+++ app/Admin/DataTables/Area/AreaDataTable.php
@@ -4,70 +4,127 @@ namespace App\Admin\DataTables\Area;
 
 use App\Admin\DataTables\BaseDataTable;
 use App\Admin\Repositories\Area\AreaRepositoryInterface;
-use Illuminate\Database\Eloquent\Builder;
+use App\Admin\Traits\GetConfig;
+use Illuminate\Database\Eloquent\Collection;
+use Yajra\DataTables\DataTableAbstract;
+use Yajra\DataTables\Exceptions\Exception;
+use Yajra\DataTables\Html\Builder;
 
 class AreaDataTable extends BaseDataTable
 {
 
-    protected $nameTable = 'areaTable';
+    use GetConfig;
+    protected $nameTable = "areaTable";
+    protected array $actions = ['reset', 'reload'];
 
     public function __construct(
         AreaRepositoryInterface $repository
     ){
-        $this->repository = $repository;
-
         parent::__construct();
 
+        $this->repository = $repository;
     }
 
-    public function setView(): void
+    public function getView(): array
     {
-        $this->view = [
+        return [
             'action' => 'admin.areas.datatable.action',
-            'name' => 'admin.areas.datatable.name'
+            'name' => 'admin.areas.datatable.name',
         ];
     }
 
-    public function setColumnSearch(): void
+
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return DataTableAbstract
+     * @throws Exception
+     */
+    public function dataTable($query): DataTableAbstract
     {
-
-        $this->columnAllSearch = [0, 1, 2];
-
-        $this->columnSearchDate = [2];
+        $this->instanceDataTable = datatables()->collection($query);
+        $this->editColumnName();
+        $this->editColumnStatus();
+        $this->editColumnCreatedAt();
+        $this->addColumnAction();
+        $this->rawColumnsNew();
+        return $this->instanceDataTable;
     }
 
     /**
      * Get query source of dataTable.
      *
+     * @return Collection
+     */
+    public function query(): Collection
+    {
+        return $this->repository->getQueryBuilderOrderBy()->get();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
      * @return Builder
      */
-    public function query(): Builder
+    public function html(): Builder
     {
-        return $this->repository->getQueryBuilderOrderBy('position', 'asc')->get();
+        $this->instanceHtml = $this->builder()
+            ->setTableId($this->nameTable)
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->dom('Bfrtip')
+            ->orderBy(0)
+            ->selectStyleSingle();
+
+        $this->htmlParameters();
+
+        return $this->instanceHtml;
     }
 
     protected function setCustomColumns(): void
     {
-        $this->customColumns = config('datatables_columns.area', []);
+        $this->customColumns = $this->traitGetConfigDatatableColumns('area');
     }
 
-    protected function setCustomEditColumns(): void
+
+    protected function editColumnName(): void
     {
-        $this->customEditColumns = [
-            'name' => $this->view['name'],
-            'created_at' => '{{ format_date($created_at) }}'
-        ];
+        $this->instanceDataTable = $this->instanceDataTable->editColumn('name', $this->view['name']);
+    }
+    protected function editColumnStatus(): void
+    {
+        $this->instanceDataTable = $this->instanceDataTable->editColumn('status', function($area){
+            return $area->status;
+        });
+    }
+    protected function editColumnCreatedAt(): void
+    {
+        $this->instanceDataTable = $this->instanceDataTable->editColumn('created_at', '{{ date("d-m-Y", strtotime($created_at)) }}');
+    }
+    protected function addColumnAction(): void
+    {
+        $this->instanceDataTable = $this->instanceDataTable->addColumn('action', $this->view['action']);
+    }
+    protected function rawColumnsNew(): void
+    {
+        $this->instanceDataTable = $this->instanceDataTable->rawColumns(['name', 'action']);
+    }
+    protected function htmlParameters(): void
+    {
+
+        $this->parameters['buttons'] = $this->actions;
+
+        $this->parameters['initComplete'] = "function () {
+
+             moveSearchColumnsDatatable('#{$this->nameTable}');
+
+            searchColumsDataTable(this);
+        }";
+
+        $this->instanceHtml = $this->instanceHtml
+            ->parameters($this->parameters);
     }
 
-    protected function setCustomAddColumns(): void
-    {
-        $this->customAddColumns = [
-            'action' => $this->view['action'],
-        ];
-    }
 
-    protected function setCustomRawColumns(): void
-    {
-        $this->customRawColumns = ['name', 'action'];
-    }
 }
