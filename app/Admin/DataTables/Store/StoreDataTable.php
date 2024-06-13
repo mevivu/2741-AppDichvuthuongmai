53eead2eb99fb4e68ac8e5ee830daf5310847c69
nguyenhoangtuananh2002 <nguyenhoangtuananh2002@gmail.com>
2024-06-13 17:17:02 +0700

    store

--- app/Admin/DataTables/Store/StoreDataTable.php
+++ app/Admin/DataTables/Store/StoreDataTable.php
@@ -3,23 +3,33 @@
 namespace App\Admin\DataTables\Store;
 
 use App\Admin\DataTables\BaseDataTable;
+use App\Admin\Repositories\Area\AreaRepositoryInterface;
 use App\Admin\Repositories\Store\StoreRepositoryInterface;
+use App\Admin\Repositories\StoreCategory\StoreCategoryRepositoryInterface;
 use App\Enums\Store\StoreStatus;
+use Illuminate\Database\Eloquent\Builder;
 
 class StoreDataTable extends BaseDataTable
 {
 
     protected $nameTable = 'storeTable';
 
+    protected AreaRepositoryInterface $areaRepository;
+    protected StoreCategoryRepositoryInterface $storeCategoryRepository;
     public function __construct(
-        StoreRepositoryInterface $repository
+        StoreRepositoryInterface $repository,
+        AreaRepositoryInterface $areaRepository,
+        StoreCategoryRepositoryInterface $storeCategoryRepository
     ){
         $this->repository = $repository;
+        $this->areaRepository = $areaRepository;
+        $this->storeCategoryRepository = $storeCategoryRepository;
 
         parent::__construct();
     }
 
-    public function setView(){
+    public function setView(): void
+    {
         $this->view = [
             'action' => 'admin.stores.datatable.action',
             'store_name' => 'admin.stores.datatable.store-name',
@@ -30,53 +40,90 @@ class StoreDataTable extends BaseDataTable
         ];
     }
 
-    public function setColumnSearch(){
+    public function setColumnSearch(): void
+    {
 
-        $this->columnAllSearch = [0, 1, 2, 3, 5, 6];
+        $this->columnAllSearch = [1, 2, 3, 5, 6];
 
-        $this->columnSearchDate = [6];
+        $this->columnSearchDate = [7];
 
         $this->columnSearchSelect = [
             [
                 'column' => 5,
                 'data' => StoreStatus::asSelectArray()
-            ]
+            ],
+        ];
+
+        $this->columnSearchSelect2 = [
+            [
+                'column' => 3,
+                'data' => $this->areaRepository->getAll()->map(function($area){
+                    return [$area->id => $area->name];
+                })
+            ],
+            [
+                'column' => 2,
+                'data' => $this->storeCategoryRepository->getAll()->map(function($store){
+                    return [$store->id => $store->name];
+                })
+            ],
+        ];
+    }
+
+    protected function setCustomFilterColumns()
+    {
+        $this->customFilterColumns = [
+            'area' => function($query, $keyword) {
+                $query->whereHas('area', function($q) use($keyword) {
+                    $q->whereIn('id', explode(',', $keyword));
+                });
+            },
+            'category' => function($query, $keyword) {
+                $query->whereHas('category', function($q) use($keyword) {
+                    $q->whereIn('id', explode(',', $keyword));
+                });
+            }
         ];
     }
 
     /**
      * Get query source of dataTable.
      *
-     * @param \App\Models\User $model
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @return Builder
      */
-    public function query()
+    public function query(): Builder
     {
-        return $this->repository->getByQueryBuilder([], ['category', 'area'], ['priority', 'desc'])->get();
+        return $this->repository->getByQueryBuilder([], ['category', 'area'], ['priority', 'desc']);
     }
 
-    protected function setCustomColumns(){
+    protected function setCustomColumns(): void
+    {
         $this->customColumns = config('datatables_columns.store', []);
     }
 
-    protected function setCustomEditColumns(){
+    protected function setCustomEditColumns(): void
+    {
         $this->customEditColumns = [
             'store_name' => $this->view['store_name'],
             'status' => $this->view['status'],
-            'category_id' => $this->view['category'],
-            'area_id' => $this->view['area'],
+            'category' => $this->view['category'],
+            'area' => $this->view['area'],
             'open_hours_1' => fn($store) => view($this->view['operating_time'], compact('store')),
             'created_at' => '{{ format_date($created_at) }}'
         ];
     }
 
-    protected function setCustomAddColumns(){
+    protected function setCustomAddColumns(): void
+    {
         $this->customAddColumns = [
             'action' => $this->view['action'],
         ];
     }
 
-    protected function setCustomRawColumns(){
-        $this->customRawColumns = ['store_name', 'category_id', 'area_id', 'open_hours_1', 'status', 'action'];
+    protected function setCustomRawColumns()
+    {
+        $this->customRawColumns = ['store_name', 'category', 'area', 'open_hours_1', 'status', 'action', 'view-product'];
+
     }
+
 }
--- resources/views/admin/stores/categories/scripts/datatable.blade.php
+++ resources/views/admin/stores/categories/scripts/datatable.blade.php
@@ -1,20 +1,31 @@
 <script>
+    function createSelectColumnUniqueDatatableAll(select, options) {
+        options.forEach(option => {
+            var opt = document.createElement("option");
+            opt.value = option;
+            opt.text = option;
+            select.appendChild(opt);
+        });
+    }
 
     function searchColumsDataTable(datatable) {
-        datatable.api().columns([0, 1, 2, 3, 4]).every(function () {
+        datatable.columns([0, 1, 2, 3, 4]).every(function () {
             var column = this;
-            var input = document.createElement("input");
-            input.setAttribute('class', 'form-control');
+            var input;
 
-            if (column.selector.cols === 3) {
+            if (column.index() === 3) {
+                input = document.createElement("input");
                 input.setAttribute('type', 'date');
-            } else if (column.selector.cols === 2) {
+                input.setAttribute('class', 'form-control');
+            } else if (column.index() === 2) {
                 input = document.createElement("select");
+                input.setAttribute('class', 'form-control');
                 createSelectColumnUniqueDatatableAll(input, @json($status));
+            } else {
+                input = document.createElement("input");
+                input.setAttribute('class', 'form-control');
+                input.setAttribute('placeholder', 'Nhập từ khóa');
             }
-
-            input.setAttribute('placeholder', 'Nhập từ khóa');
-
 
             $(input).appendTo($(column.footer()).empty())
                 .on('change', function () {
@@ -24,9 +35,17 @@
     }
 
     $(document).ready(function () {
-        // define columns for the datatables
+        // Check if the DataTable is initialized and accessible
+        if (window.LaravelDataTables && window.LaravelDataTables["storeCatTable"]) {
+            var datatable = window.LaravelDataTables["storeCatTable"];
 
-        columns = window.LaravelDataTables["storeCategoryTable"].columns();
-        toggleColumnsDatatable(columns);
+            // Add search inputs to the columns once the table is initialized
+            datatable.on('init.dt', function () {
+                searchColumsDataTable(datatable);
+                toggleColumnsDatatable(datatable.columns());
+            });
+        } else {
+            console.error("DataTable 'storeCatTable' is not defined.");
+        }
     });
 </script>
