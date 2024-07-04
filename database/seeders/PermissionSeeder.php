0a91b282eb13e353538fea80bc06edbb3cf7cd60
Zed23131414 <hophat777@gmail.com>
2024-07-04 12:14:51 +0700

    The third Commit

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -239,7 +239,7 @@ class PermissionSeeder extends Seeder
         ]);
 
         // Module Category_Systems
-        DB::table('modules')->insertGetId([
+        DB::table('modules')->insert([
             'id' => 22,
             'name' => 'Quản lý Dịch Vụ',
             'description' => '<p>Chức năng quản lý Dịch Vụ</p>',
@@ -1258,6 +1258,22 @@ class PermissionSeeder extends Seeder
             'created_at' => DB::raw('NOW()'),
             'updated_at' => DB::raw('NOW()')
         ]);
+        DB::table('role_has_permissions')->insert([
+            'permission_id' => 87,
+            'role_id' => 1
+        ]);
+        DB::table('role_has_permissions')->insert([
+            'permission_id' => 88,
+            'role_id' => 1
+        ]);
+        DB::table('role_has_permissions')->insert([
+            'permission_id' => 89,
+            'role_id' => 1
+        ]);
+        DB::table('role_has_permissions')->insert([
+            'permission_id' => 90,
+            'role_id' => 1
+        ]);
         // End permission categories system
 
         //seeding model_has_roles
@@ -1451,6 +1467,5 @@ class PermissionSeeder extends Seeder
             'permission_id' => 46,
             'role_id' => 1
         ]);
-        // ** Start permision Category System **//
     }
 }
