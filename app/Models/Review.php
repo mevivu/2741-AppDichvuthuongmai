0420da2b610a839ffa963474206a188d71d2ef57
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-07-26 11:32:16 +0700

    create migrate reviews

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
@@ -11,13 +12,15 @@ class Review extends Model
 
     protected $table = "reviews";
 
-    protected $guarded = [];
+    protected $fillable = [
+        'user_id',
+        'stars',
+        'content',
+    ];
 
-    public function user(){
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
-    public function scopeAuthCurrent($query){
-        return $query->where('user_id', auth('sanctum')->user()->id);
-    }
 }
