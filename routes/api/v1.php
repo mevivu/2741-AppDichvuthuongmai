136058e575e4f99f1c5192f64f78b414b13d5dd6
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-07-30 16:58:07 +0700

    fix v1.php

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -20,18 +20,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::controller(App\Api\V1\Http\Controllers\Discount\DiscountController::class)->prefix('/discounts')
-->as('discount.')
-->group(function () {
-    Route::get('/', 'index')->name('index'); // Route xuất ra danh sách các discount
-    Route::get('/show/{id}', 'show')->name('show'); // Route xem chi tiết 1 discount, tham số là {id} là discountid
-    Route::get('/store/{storeId}', 'getByStore')->name('getByStore'); // Route lấy danh sách discount theo store_id
-    Route::get('/user/{userId}', 'getByUser')->name('getByUser');
-    Route::get('/driver/{driverId}', 'getByDriver')->name('getByDriver');
-    // Route::get('/product/{productId}', 'getByProduct')->name('getByProduct');
-    Route::get('/store/{storeId}/discount/{discountId}', 'getDiscountByStoreAndId')->name('getDiscountByStoreAndId');
-    Route::get('/product/{productId}', 'getByProduct')->name('getByProduct');
-
-Route::controller(App\Api\V1\Http\Controllers\Discount\DiscountController::class)->prefix('/discounts')
     ->as('discount.')
     ->group(function () {
         Route::get('/', 'index')->name('index'); // Route xuất ra danh sách các discount
@@ -42,6 +30,7 @@ Route::controller(App\Api\V1\Http\Controllers\Discount\DiscountController::class
         // Route::get('/product/{productId}', 'getByProduct')->name('getByProduct');
         Route::get('/store/{storeId}/discount/{discountId}', 'getDiscountByStoreAndId')->name('getDiscountByStoreAndId');
         Route::get('/product/{productId}', 'getByProduct')->name('getByProduct');
+
     });
 //***** -- discount -- ******* //
 //store
@@ -85,12 +74,14 @@ Route::prefix('drivers')->controller(DriverController::class)
         Route::post('/register', 'register')->name('register');
         Route::post('/logout', 'logout')->name('logout');
         Route::post('/refresh', 'refresh')->name('refresh');
+
     });
 
 //auth
 Route::prefix('auth')->controller(AuthController::class)
     ->group(function () {
         Route::post('/login', 'login')->name('login');
+
     });
 
 
@@ -101,8 +92,6 @@ Route::prefix('orders')->controller(OrderController::class)
         Route::post('/rent-car', 'createRentOrder')->name('createRentOrder');
         Route::delete('/{id}', 'delete')->name('delete');
     });
-
-
 
 
 //post category
@@ -133,18 +122,6 @@ Route::controller(App\Api\V1\Http\Controllers\Topping\ToppingController::class)
         Route::delete('/delete', 'delete')->name('delete');
         Route::post('/add', 'add')->name('add');
         Route::put('/edit', 'edit')->name('edit');
-    });
-
-//products
-Route::controller(App\Api\V1\Http\Controllers\Product\ProductController::class)
-    ->prefix('/products')
-    ->as('product.')
-    ->group(function () {
-        Route::get('/', 'index')->name('index');
-        Route::get('/{id}', 'show')->name('show');
-        Route::delete('/{id}', 'delete')->name('delete');
-        Route::post('/', 'store')->name('store');
-        Route::put('/', 'update')->name('update');
     });
 //review product
 Route::controller(App\Api\V1\Http\Controllers\Review\ReviewController::class)
@@ -234,6 +211,7 @@ Route::prefix('/product')
                         Route::get('/show', 'show')->name('show');
                     });
             });
+
     });
 
 //slider
