114a571468d9105ca5c16ad679fa4cd28c3333f8
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-07-30 17:02:27 +0700

    fix admin.php

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -235,11 +235,11 @@ Route::controller(App\Api\V1\Http\Controllers\Topping\ToppingController::class)
     ->prefix('/toppings')
     ->as('topping.')
     ->group(function () {
-        Route::get('/', 'index')->name('index'); // Route xuất ra danh sách các Topping
-        Route::get('/show/{id}', 'show')->name('show'); // Route xem chi tiết 1 Topping, tham số là {id} là toppongid
-        Route::delete('/delete', 'delete')->name('delete'); // Route xóa 1 Phòng
-        Route::post('/add', 'add')->name('add'); // Route thêm 1 Phòng mới
-        Route::put('/edit', 'edit')->name('edit'); // Route sửa thông tin 1 Phòng
+        Route::get('/', 'index')->name('index');
+        Route::get('/show/{id}', 'show')->name('show');
+        Route::delete('/delete', 'delete')->name('delete');
+        Route::post('/add', 'add')->name('add');
+        Route::put('/edit', 'edit')->name('edit');
     });
 //***** -- Category System -- ******* //
 Route::controller(App\Api\V1\Http\Controllers\CategorySystem\CategorySystemController::class)
