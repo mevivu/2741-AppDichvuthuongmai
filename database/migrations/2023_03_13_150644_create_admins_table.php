6887a2a5293c09d6a8cb720411ea9a9ff604e17e
Nguyễn Minh Huy <minhhuyblld@gmail.com>
2024-08-02 15:39:52 +0700

    update key

--- database/migrations/2023_03_13_150644_create_admins_table.php
+++ database/migrations/2023_03_13_150644_create_admins_table.php
@@ -26,6 +26,7 @@ return new class extends Migration
             $table->text('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->string('device_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
